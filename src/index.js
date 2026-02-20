69bd31370cdad6ebc51c34fb58738ecefc180ad6
Eemeli Mark <eemeli.mark@vertex.fi>
2026-02-20 10:47:29 +0200

    Fix resume

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import initTilt from './js/tilt';
 import initSr from './js/sr';
 import './style/main.scss';
+import resumePdf from './assets/Resume.pdf';
+
+const resumeLink = document.getElementById('resume-link');
+if (resumeLink) {
+  resumeLink.href = resumePdf;
+}
 
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
