46f877fb808ba3765f7d7aa4df9fac633ef4ca7a
Eemeli Mark <eemelijoonatan@gmail.com>
2026-06-22 16:09:13 +0300

    Tweaked latency

--- src/js/sr.js
+++ src/js/sr.js
@@ -54,7 +54,7 @@ export default function() {
   /* Contact Section */
   ScrollReveal().reveal('.contact-wrapper', {
     ...defaultProps,
-    delay: 200,
+    delay: 300,
     origin: 'bottom'
   });
 }
